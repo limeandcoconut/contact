2fe23a153fb08561b75ddb64169ceeab3717b6e0
Jacob Smith <js@jacobsmith.tech>
2020-07-12 10:46:24 -0700

    Rewrap comments

--- contact.js
+++ contact.js
@@ -12,7 +12,6 @@ const brentsMethod = require('brents-method')
  * TOI = Time of intercept
  */
 
-// TODO: I think this is ascending
 /**
  * Sort separating information objects in descending order by distance.
  * @function distanceDescending
@@ -30,8 +29,8 @@ const distanceDescending = (a, b) => {
  * @param   {Vector2D}  direction   The vector along which to find supporting vertices.
  * @param   {Shape2D}   shapeA      One of the shapes.
  * @param   {Shape2D}   shapeB      The other shape.
- * @param   {Array}     vertices    The array of Vector2D vertices representing the feature of the Minkowski
- *                                  Difference closeset to the origin. This is here to be mutated.
+ * @param   {Array}     vertices    The array of Vector2D vertices representing the feature of the Minkowski Difference
+ *                                  closeset to the origin. This is here to be mutated.
  * @return  {Vector2D}              The new vertex ont the Minkowski Difference.
  */
 const addSupportVertex = (direction, shapeA, shapeB, vertices) => {
@@ -71,9 +70,9 @@ const pruneVertices = (vertices) => {
     return vertices
   }
 
+  // Sort ascending
   vertices.sort((a, b) => a.magnitude() - b.magnitude())
-  // vertices is reassigned here just in case the user wants to use the mutated version rather than the returned
-  // one.
+  // vertices is reassigned here just in case the user wants to use the mutated version rather than the returned one.
   vertices = vertices.slice(0, 2)
   return vertices
 }
@@ -121,8 +120,7 @@ const gjk = (shapeA, shapeB) => {
     // If there are more than two vertices return the two closeset to the origin as our closest feature.
     vertices = pruneVertices(vertices)
 
-    // Find the distance from the closest feature to the origin. This uses vector projection clamped to a
-    // line segment.
+    // Find the distance from the closest feature to the origin. This uses vector projection clamped to a line segment.
     // See Figure 1 for a diagram of below.
     v1 = vertices[0]
     v2 = vertices[1]
@@ -134,8 +132,8 @@ const gjk = (shapeA, shapeB) => {
     // Line segment from v1 to v2
     v1v2 = Vector2D.subtract(v2, v1)
 
-    // Project the vector from v1 to the origin along the segment v1v2. Clamp the projection to the line
-    // segment so that if it doesn't fall in the middle either v1 or v2 will be used to calculate the
+    // Project the vector from v1 to the origin along the segment v1v2. Clamp the projection to the line segment so that
+    // if it doesn't fall in the middle either v1 or v2 will be used to calculate the
     // Minkowski Distance. This is the new direction. Again see Figure 1 for a diagram.
     direction = Vector2D.projectVector(v1.getInverse(), v1v2, true)
     direction = Vector2D.add(direction, v1)
@@ -181,8 +179,8 @@ const supportingHyperspaceExtrusion = (supportingDistances, shapeA, shapeB) => {
 }
 
 /**
- * Perform an intersect test on two shapes with given velocities across a time interval represented by the
- * velocities. If necessary get the time of intercept for the two shapes.
+ * Perform an intersect test on two shapes with given velocities across a time interval represented by the velocities.
+ * If necessary get the time of intercept for the two shapes.
  * @function test
  * @param   {Shape2D}           shapeA          One of the shapes to perform the intersect test on.
  * @param   {Shape2D}           shapeB          Another shape to perform the intersect test on.
@@ -190,8 +188,8 @@ const supportingHyperspaceExtrusion = (supportingDistances, shapeA, shapeB) => {
  * @param   {Vector2D}          shapeBVelocity  The velocity of shapeB.
  * @return  {Boolean|Number}                    If the shapes intersect initially return true.
  *                                              If the shapes do not intersect at all return false.
- *                                              If necessary return a Number on the interval [0, 1] representing
- *                                              a fractional time of intercept.
+ *                                              If necessary return a Number on the interval [0, 1] representing a
+ *                                              fractional time of intercept.
  */
 const test = (shapeA, shapeB, shapeAVelocity, shapeBVelocity) => {
   if (!(shapeA instanceof Shape2D)) {
@@ -207,8 +205,8 @@ const test = (shapeA, shapeB, shapeAVelocity, shapeBVelocity) => {
     throw new TypeError(`Shape B velocity must be an instance of Vector2D. ${shapeBVelocity.constructor.name} provided.`)
   }
   /*
-  * Get supporting directions and distances at t0 to see if there is an initial separating distance (positive
-  * supporting distance).
+  * Get supporting directions and distances at t0 to see if there is an initial separating distance (positive supporting
+  * distance).
   * Supporting distances at t0 are sorted descending.
   * These are objects with distance and direction.
   * Distances are signed as they correspond to a direction.
@@ -263,12 +261,12 @@ const test = (shapeA, shapeB, shapeAVelocity, shapeBVelocity) => {
     */
 
   // We've found a intersect time so return it.
-  // This can be used as a scalar for the velocities of shapeA and shapeB to provide movement to the exact place
-  // that they intercept.
+  // This can be used as a scalar for the velocities of shapeA and shapeB to provide movement to the exact place that
+  // they intercept.
   return brentsMethod({
     // This is our function for finding time of intercept.
-    // It takes t and returns the maximum supporting distance at that time based on the supporting directions
-    // that were found earlier.
+    // It takes t and returns the maximum supporting distance at that time based on the supporting directions that were
+    // found earlier.
     func: (t) => {
       // Scale v by t.
       let vt = new Vector2D(relativeVelocity)
@@ -279,13 +277,12 @@ const test = (shapeA, shapeB, shapeAVelocity, shapeBVelocity) => {
       // Get supporting distances for shapeA and shapeBt using direction vectors from inital supporting distances.
       let supportingDistancest = supportingHyperspaceExtrusion(initialSupportingDirections, shapeA, shapeBt)
 
-      // If the support distance is positive then a simple extrusion test wont be precise enough,
-      // we need to run a full GJK extrusion to get the closest distance.
+      // If the support distance is positive then a simple extrusion test wont be precise enough, we need to run a full
+      // GJK extrusion to get the closest distance.
       if (supportingDistancest[0].distance > 0) {
         supportingDistancest = gjk(shapeA, shapeBt)
         // Go ahead and use the new supporting directions to calculate future distances.
-        // It's good to use the refined directions from this t because we know that P and Q are disjoint from
-        // t0 to now.
+        // It's good to use the refined directions from this t because we know that P and Q are disjoint from t0 to now.
         supportingDistancest = supportingDistancest.filter(({ distance }) => distance > 0)
         initialSupportingDirections = supportingDistancest
       }
